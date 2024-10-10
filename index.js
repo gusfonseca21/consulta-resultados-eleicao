c97d5e9dd857dfd5a1ba4927dac60be66f63f0b7
Gustavo <gusfonseca.dev@gmail.com>
2024-10-09 23:49:33 -0300

    Avançando

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const seletorCandidaturaEl = document.querySelector(".seletor-candidatura");
 const seletorEstadoEl = document.querySelector(".seletor-estado");
 const seletorMunicipioEl = document.querySelector(".seletor-municipio");
 const botaoCarregarEl = document.querySelector(".carregar");
+const tituloColVice = document.querySelector(".vice-linha");
+const tabelaEl = document.querySelector(".tabela");
+const tabelaCorpo = document.querySelector(".tabela tbody");
 
 let candidaturaSelecionada = "";
 let estadoSelecionado = "";
@@ -11,7 +14,6 @@ let municipioSelecionado = "";
 
 seletorCandidaturaEl.addEventListener("change", (value) => {
   const valor = value.target.value;
-  console.log("valor", valor);
   if (!valor) {
     seletorEstadoEl.disabled = true;
     seletorMunicipioEl.disabled = true;
@@ -52,6 +54,7 @@ seletorMunicipioEl.addEventListener("change", (value) => {
 
 function retornaMunicipios(siglaEstadoSelecionado) {
   // Importando array com nome dos municípios e seu código do TSE
+  // eslint-disable-next-line no-undef
   axios.get("./municipios_brasileiros_tse.json").then((resposta) => {
     const arrayOpcoesMunicipios = resposta.data
       .map((municipio) => {
@@ -61,7 +64,6 @@ function retornaMunicipios(siglaEstadoSelecionado) {
           if (codigo_municipio.length < 5) {
             codigo_municipio = codigo_municipio.padStart(5, "0");
           }
-          console.log("codigo_municipio", codigo_municipio);
           opcaoMunicipioEl.value = codigo_municipio;
           opcaoMunicipioEl.textContent = municipio.nome_municipio;
           return opcaoMunicipioEl;
@@ -86,12 +88,24 @@ function limparOpcoes(elementoASerLimpo) {
 const ENDPOINT_BASE =
   "https://resultados.tse.jus.br/oficial/ele2024/619/dados/{ESTADO}/{ESTADO_COD_MU}-{TIPO_CANDIDATURA}-e000619-u.json";
 
-botaoCarregarEl.addEventListener("click", () => {
+botaoCarregarEl.addEventListener("click", async () => {
+  const linhasEl = tabelaEl.children;
+  limparTabela();
+  console.log("linhasEl", linhasEl);
   const endpointFinal = ENDPOINT_BASE.replace("{ESTADO}", estadoSelecionado)
     .replace("{ESTADO_COD_MU}", estadoSelecionado + municipioSelecionado)
     .replace("{TIPO_CANDIDATURA}", candidaturaSelecionada);
 
-  const dados = axios.get(endpointFinal).then((resultado) => resultado.data);
+  if (candidaturaSelecionada === "c0011") {
+    tituloColVice.style.display = "table-cell";
+  } else {
+    tituloColVice.style.display = "none";
+  }
+
+  // eslint-disable-next-line no-undef
+  const resultado = await axios.get(endpointFinal);
+
+  const dados = resultado.data;
 
   // Agremiações são as junções de vários partidos. Contém um array com os partidos que fazem parte da agremiação, que contém um array com os candidados do partido
   const agremiacoes = dados?.carg[0]?.agr;
@@ -100,32 +114,50 @@ botaoCarregarEl.addEventListener("click", () => {
     agr.par.forEach((partido) => {
       const sigla_partido = partido.sg;
       partido.cand.forEach((candidato) => {
-        let nome_candidato = candidato.nmu
-          .replaceAll(",", "")
-          .replace(";", "")
-          .replace(/&#09;/g, "");
-        // nome_candidato = he.decode(nome_candidato);
+        const nome_candidato = formatarNome(candidato.nmu);
 
-        const linha_tabela = [
+        const linha = [
           nome_candidato,
           sigla_partido,
-          candidato.n,
-          nome_municipio,
-          sigla_estado.toUpperCase(),
-          candidato.dt,
           candidato.dvt,
           candidato.st,
           candidato.vap,
-          candidato.pvap,
+          candidato.pvap + " %",
         ];
 
-        if (values.candidatura === "prefeito") {
-          const nome_vice = he.decode(candidato.vs[0].nmu.replace(",", ""));
-          linha_tabela.push(nome_vice);
+        // Se for prefeito, adicionar nome do vice
+        if (candidaturaSelecionada === "c0011") {
+          const nome_vice = formatarNome(candidato.vs[0].nmu);
+          linha.push(nome_vice);
         }
 
-        tabela.push(linha_tabela);
+        const linhaTabelaEl = document.createElement("tr");
+
+        const dadosLinhaEl = linha.map((dadoColuna) => {
+          const colunaEl = document.createElement("td");
+          colunaEl.textContent = dadoColuna;
+          return colunaEl;
+        });
+
+        dadosLinhaEl.forEach((linhaEl) => {
+          linhaTabelaEl.appendChild(linhaEl);
+        });
+
+        tabelaEl.appendChild(linhaTabelaEl);
+        tabelaEl.style.display = "table";
       });
     });
   }
 });
+
+function formatarNome(nome) {
+  return nome.replaceAll(",", "").replace(";", "").replace(/&#09;/g, "");
+}
+
+function limparTabela() {
+  const linhasEl = tabelaEl.children;
+  for (let i = linhasEl.length - 1; i > 0; i--) {
+    console.log("linhasEl[i]", linhasEl[i].textContent);
+    linhasEl[i].remove();
+  }
+}
